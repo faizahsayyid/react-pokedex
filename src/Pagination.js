6a9b4c226716bb79034ebff43713fd4705031588
faizahsayyid <faizah.sayyid@gmail.com>
2021-06-21 19:22:23 -0400

    fix: created react app with pokemon api

---
+++ src/Pagination.js
@@ -0,0 +1,11 @@
+const Pagination = ({goToNextPage, goToPrevPage }) => {
+    return (
+        <div>
+            {goToPrevPage && <button onClick={goToPrevPage}>Previous</button>}
+            {goToNextPage && <button onClick={goToNextPage}>Next</button>}
+        </div>
+    )
+}
+
+export default Pagination
+
