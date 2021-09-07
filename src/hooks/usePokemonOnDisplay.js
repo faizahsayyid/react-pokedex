c2fd65710d7c978c0da897d8c18a3e016274cdee
faizahsayyid <faizah.sayyid@gmail.com>
2021-09-07 17:33:38 -0400

    fix: adding error checking on pokemon side image

--- src/hooks/usePokemonOnDisplay.js
+++ src/hooks/usePokemonOnDisplay.js
@@ -1,22 +1,31 @@
-import axios from 'axios'
-import { useEffect, useContext, useState} from 'react'
-import { PokemonListContext } from '../contexts/PokemonListContext'
+import axios from "axios";
+import { useEffect, useContext, useState } from "react";
+import { PokemonListContext } from "../contexts/PokemonListContext";
 
 const usePokemonOnDisplay = () => {
-    const { pokemonOnDisplay } = useContext(PokemonListContext)
-    const [pokemonDetails, setPokemonDetails] = useState()
-    const [loading, setLoading] = useState(true)
+  const { pokemonOnDisplay, setError } = useContext(PokemonListContext);
+  const [pokemonDetails, setPokemonDetails] = useState();
+  const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        setLoading(true)
-        axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonOnDisplay}`)
-            .then(res => {
-                setPokemonDetails(res.data)
-                setLoading(false)
-            })
-    }, [pokemonOnDisplay]) 
+  useEffect(() => {
+    let cancel;
+    setLoading(true);
+    axios
+      .get(`https://pokeapi.co/api/v2/pokemon/${pokemonOnDisplay}`, {
+        cancelToken: new axios.CancelToken((c) => (cancel = c)),
+      })
+      .then((res) => {
+        setPokemonDetails(res.data);
+        setLoading(false);
+      })
+      .catch((e) => {
+        if (axios.isCancel(e)) return;
+        setError(e);
+      });
+    return () => cancel();
+  }, [pokemonOnDisplay, setError]);
 
-    return {loading, pokemonDetails}
-}
+  return { loading, pokemonDetails };
+};
 
-export default usePokemonOnDisplay
+export default usePokemonOnDisplay;
