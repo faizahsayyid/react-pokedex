520f55208409962d279e887e15245b8f341c4531
Faizah Sayyid <57816131+faizahsayyid@users.noreply.github.com>
2022-02-01 12:26:32 -0500

    Faizah/pokemon details (#10)
    * fix: working on pokemon details
    
    * pokemon details for now

--- src/components/PokemonSideImage/PokemonSideImage.js
+++ src/components/PokemonSideImage/PokemonSideImage.js
@@ -1,46 +1,19 @@
 import React, { useContext } from "react";
 import usePokemonOnDisplay from "../../hooks/usePokemonOnDisplay";
-import LoadingSpinner from "../LoadingSpinner/LoadingSpinner";
-import "./PokemonSideImage.css";
-import Error from "../Error/Error";
 import { PokemonListContext } from "../../contexts/PokemonListContext";
-import Types from "../Types/Types";
+import PokemonImage from "../PokemonImage/PokemonImage";
 
 const PokemonSideImage = () => {
   const { loading, pokemonDetails } = usePokemonOnDisplay();
   const { error, pokemonOnDisplay } = useContext(PokemonListContext);
 
   return (
-    <div className="pokemon-side-image-container">
-      <div className="pokemon-display-details">
-        {!error ? (
-          <p>{!loading && pokemonDetails.name}</p>
-        ) : (
-          <Error message={`Could not load ${pokemonOnDisplay}`} />
-        )}
-      </div>
-      <div className="pokemon-image">
-        <span className="bar"></span>
-        <span className="bar"></span>
-        <span className="bar"></span>
-        <span className="bar"></span>
-        <span className="bar"></span>
-        <span className="bar"></span>
-        {loading ? (
-          <LoadingSpinner height={240} width={240} />
-        ) : (
-          <img
-            className="psi-sprite"
-            src={pokemonDetails.sprites.front_default}
-            alt={`${pokemonDetails.name} sprite`}
-          />
-        )}
-      </div>
-      <div className="pokemon-display-details taller">
-        Types:
-        {!loading && <Types typesCollection={pokemonDetails.types} />}
-      </div>
-    </div>
+    <PokemonImage
+      loading={loading}
+      pokemonDetails={pokemonDetails}
+      error={error}
+      pokemonOnDisplay={pokemonOnDisplay}
+    />
   );
 };
 
